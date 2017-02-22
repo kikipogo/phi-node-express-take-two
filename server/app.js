92d79348eb0753ac29a22f6807788f92094d1f24
Christine Pogatchnik <christinepogatchnik@Christines-MacBook-Pro.local>
2017-02-22 17:44:24 -0600

    Base Mode

--- server/app.js
+++ server/app.js
@@ -2,20 +2,51 @@ console.log('Starting up the server');
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
 
 app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
-var fishiesList = [{name: 'walleye'}, {name: 'pike'}];
+var fishiesList = [{name: 'walleye'}, {name: 'pike'}, {name: 'muskie'}];
 
 app.get('/fish', function(req, res){
   res.send(fishiesList);
 });
 
-// handle the request for the first fish
+app.post('/fish/new', function(req, res){
+  var newFish = req.body;
+  if(newFish.name !== ''){
+  console.log('new fish is:', newFish);
+  fishiesList.push(newFish);
+  res.sendStatus(200);
+} else {
+  res.sendStatus(400);
+  console.log('else statement');
+}
+});
 
+// handle the request for the first fish
+app.get('/fish/first', function(req, res){
+  res.send(fishiesList[0]);
+});
+
+// handle the request for the first fish name
+app.get('/fish/first/name', function(req, res){
+  res.send(fishiesList[0].name);
+});
 
 // handle the request for the last fish
+app.get('/fish/last', function(req, res){
+  var lastIndex = fishiesList.length -1;
+  res.send(fishiesList[lastIndex]);
+});
 
+// handle the request for the last fish name
+app.get('/fish/last/name', function(req, res){
+  var lastIndex = fishiesList.length -1;
+  res.send(fishiesList[lastIndex].name);
+});
 
 
 app.listen(5000);
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,11 +1,49 @@
 console.log('sourced!');
 $(document).ready(function(){
   console.log('jquery was correctly sourced!');
+
+  getFishData();
+function getFishData(){
   $.ajax({
     type: 'GET',
     url: '/fish',
     success: function(response) {
-      console.log('response', response);
+      console.log('response', response); // returns fishList array
+      $('#fishTank').empty();
+      for (var i = 0; i < response.length; i++) { //response is the array
+        $('#fishTank').append('<li>' + response[i].name + '</li>'); //append fish name to li in DOM
+      }
     }
   });
+
+  $.ajax({
+    type: 'GET',
+    url: '/fish/first/name',
+    success: function(response) {
+      console.log('response', response); // returns fishList array
+        $('#fishFishy').text(response);
+      }
+  });
+} //
+
+  $('#newFishButton').on('click', function(){
+    var newFishObject = {};
+    newFishObject.name = $('#newFishName').val();
+    $.ajax({
+      type: 'POST',
+      url: '/fish/new',
+      data: newFishObject,
+      success: function(response) {
+        console.log(response);
+        getFishData();
+      },
+      error: function(error){
+      alert('ERROR: You did not enter a fish!');
+    }
+    });
+  });
+
+
+
+
 });
